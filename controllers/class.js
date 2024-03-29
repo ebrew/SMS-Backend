1bf98cc2e0874ed3a5b76de963cb5352d21f4165
Emmanuel Brew <emmanuelbrew85@gmail.com>
2024-03-29 12:02:05 +0000

    resolving fetching classes issue

--- controllers/class.js
+++ controllers/class.js
@@ -81,7 +81,7 @@ exports.addClass = async (req, res) => {
 };
 
 // Get all Classes
-exports.allClasses = async (req, res) => {
+exports.allClasses1 = async (req, res) => {
   passport.authenticate("jwt", { session: false })(req, res, async (err) => {
     if (err)
       return res.status(401).json({ message: 'Unauthorized' });
@@ -180,5 +180,83 @@ exports.allClasses = async (req, res) => {
   console.error('Error:', error);
   return res.status(500).json({ message: "Can't fetch data at the moment!" });
 }
+  });
+};
+exports.allClasses = async (req, res) => {
+  passport.authenticate("jwt", { session: false })(req, res, async (err) => {
+    if (err)
+      return res.status(401).json({ message: 'Unauthorized' });
+
+    try {
+      const classSections = await Section.findAll({
+        attributes: ['id', 'name', 'capacity'],
+        include: {
+          model: Class,
+          attributes: ['id', 'name', 'grade'],
+          order: [['grade', 'ASC']],
+          include:  {
+            model: User,
+            attributes: ['id', 'firstName', 'lastName'],
+          },
+        },
+      });
+
+      // Mapping the result to the desired format
+      const formattedResult2 = classSections.map(data => {
+        let headTeacher = "Unknown"; // Default value in case head teacher is null
+
+        // Check if the user object exists before accessing its properties
+        if (data.Class.User) {
+          headTeacher = `${data.Class.User.firstName} ${data.Class.User.lastName}`;
+        }
+
+        return {
+          classSectionId: data.id,
+          classSection: `${data.Class.name} ${data.name}`,
+          capacity: data.capacity,
+          grade: data.Class.grade,
+          headTeacher: headTeacher,
+          class: {
+            id: data.Class.id,
+            name: data.Class.name,
+            grade: data.Class.grade,
+            headTeacher: {
+              id: data.Class.User ? data.Class.User.id : null,
+              firstName: data.Class.User ? data.Class.User.firstName : null,
+              lastName: data.Class.User ? data.Class.User.lastName : null
+            },
+          },
+        };
+      });
+
+      const classes = await Class.findAll({ order: [['grade', 'ASC']], include:  { model: User, attributes: ['id', 'firstName', 'lastName'] } });
+
+      // Map through activeAssignedTeachers and create promises to fetch subjects
+      const promises = classes.map(async (data) => {
+        const sections = await Section.findAll({
+          where: { classId: data.id },
+          attributes: ['id', 'name', 'capacity'],  
+        });
+      
+        // Return the formatted data along with the subjects
+        return {
+          id: data.id,
+          name: data.name,
+          grade: data.grade,
+          createdAt: data.createdAt,
+          updatedAt: data.updatedAt,
+          headTeacher: data.User ? `${data.User.firstName} ${data.User.lastName}` : null,
+          classSections: sections,
+        };
+      });
+      
+      // Execute all promises concurrently and await their results
+      const formattedResult1 = await Promise.all(promises);
+
+      return res.status(200).json({ 'class - option 1': formattedResult1, 'classes - option 2':  formattedResult2 });
+    } catch (error) {
+      console.error('Error:', error);
+      return res.status(500).json({ message: "Can't fetch data at the moment!" });
+    }
   });
 };
